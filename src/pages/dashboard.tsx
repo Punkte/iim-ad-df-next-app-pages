8e6bcc98db625f6e7ebf7fa83b98a45e1c3e339f
Teepan <paartheepan94@gmail.com>
2025-01-27 12:27:21 +0100

    add todo page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export type TodoItem = {
+  completed: boolean;
+  id: number;
+  title: string;
+  userId: number
+}
+
+type Props = {
+  data: TodoItem[]
+}
+const Dashboard = ({data}: Props) => {
+  console.log(global.window)
+  return (
+    <>
+      <div>Dashboard</div>
+      <ul>
+        {data.map(element => {
+          return <li key={element.id}>{element.title}</li>
+        })}
+      </ul>
+    </>
+  )
+}
+
+export const getStaticProps = async () => {
+  console.log('here')
+  const req = await fetch('https://jsonplaceholder.typicode.com/todos')
+  const res = await req.json()
+
+  return {
+    props: {
+      data: res
+    }
+  }
+}
+
+export default Dashboard
---
+++ src/pages/dashboard/[id].tsx
@@ -0,0 +1,42 @@
+import { GetStaticPaths, GetStaticProps } from "next"
+import { TodoItem } from "../dashboard"
+import { ParsedUrlQuery } from "querystring"
+
+interface Params extends ParsedUrlQuery {
+  id: string
+}
+
+interface Props {
+  data: TodoItem
+}
+const TodoPage = ({data}: Props) => {
+  console.log(data)
+  return (
+    <div>{JSON.stringify(data)}</div>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const req = await fetch('https://jsonplaceholder.typicode.com/todos')
+  const res = await req.json() as TodoItem[]
+
+  const paths = res.map((todo) => ({
+    params: {id: `${todo.id}`}
+  }))
+
+  return {paths, fallback: 'blocking'}
+}
+
+export const getStaticProps: GetStaticProps<Props, Params> = async ({params}) => {
+  const req = await fetch(`https://jsonplaceholder.typicode.com/todos/${params?.id}`)
+  const res = await req.json()
+
+  return {
+    props: {
+      data: res
+    }
+  }
+}
+
+
+export default TodoPage
